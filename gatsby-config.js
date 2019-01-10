1d2d818ba3ec9608965e710accd6cba07b5ed7dd
gleen <gleen.lexry@n2nconnect.com>
2019-01-10 10:11:09 +0800

    added Google analytics.. not sure if works

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-styled-components'
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: "UA-118401527-2",
+        head: true
+      },
     }
   ],
 }
