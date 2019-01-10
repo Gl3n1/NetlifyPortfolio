c8606aeb8270046d34c3a4c55eb246e650538bc3
gleen <gleen.lexry@n2nconnect.com>
2019-01-10 17:39:57 +0800

    test cacat form

--- src/pages/components/Form.js
+++ src/pages/components/Form.js
@@ -1,98 +1,79 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { navigateTo } from "gatsby-link";
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
-  },
-  button: {
-    margin: theme.spacing.unit,
-    width: 100
-  },
-  input: {
-    display: 'none',
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-});
 
-class Form extends React.Component {
-  state = {};
-
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value
-    });
-    console.log(this.state)
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": form.getAttribute("name"),
-  //       ...this.state
-  //     })
-  //   })
-  //     .then(() => navigateTo(form.getAttribute("action")))
-  //     .catch(error => alert(error));
-  // };
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
   render() {
-    const { classes } = this.props;
-
     return (
-      <form className={classes.container} noValidate autoComplete="off" 
-      name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field"
-      onSubmit={this.handleSubmit}>
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
+      <div>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <div hidden>
             <label>
               Don’t fill this out:{" "}
               <input name="bot-field" onChange={this.handleChange} />
             </label>
-          </p>
-        <TextField
-          id="standard-name"
-          label="Name"
-          className={classes.textField}
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-          required={true}
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Multiline"
-          multiline
-          rows="4"
-          className={classes.textField}
-          margin="normal"
-          required={true}
-          onChange={this.handleChange('message')}
-          value={this.state.message}
-        />
-        <Button variant="contained" color="primary" className={classes.button} type="submit">
-          Submit
-        </Button>
-      </form>
+          </div>
+          <div>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Send</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
-
-Form.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Form);
