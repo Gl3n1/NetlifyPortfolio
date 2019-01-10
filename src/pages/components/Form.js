9a2ef79c8c5f119b152875090ddfe58ea84ee9da
gleen <gleen.lexry@n2nconnect.com>
2019-01-10 17:52:00 +0800

    updated form

--- src/pages/components/Form.js
+++ src/pages/components/Form.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { navigateTo } from "gatsby-link";
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit,
+    width: 100
+  },
+  input: {
+    display: 'none',
+  }
+});
 
 function encode(data) {
   return Object.keys(data)
@@ -7,14 +30,14 @@ function encode(data) {
     .join("&");
 }
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+class Form extends React.Component {
+  state = {};
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+    console.log(this.state)
   };
 
   handleSubmit = e => {
@@ -33,47 +56,68 @@ export default class Contact extends React.Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <form
-          name="contact"
-          method="post"
-          action="/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <div hidden>
+      <form 
+      className={classes.container} 
+      autoComplete="off" 
+      name="contact" 
+      method="POST"
+      validate="true"
+      data-netlify="true" 
+      data-netlify-honeypot="bot-field"
+      onSubmit={this.handleSubmit}>
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
             <label>
               Don’t fill this out:{" "}
               <input name="bot-field" onChange={this.handleChange} />
             </label>
-          </div>
-          <div>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <button type="submit">Send</button>
-          </div>
-        </form>
-      </div>
+          </p>
+        <TextField
+          id="standard-name"
+          name="name"
+          label="Name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+          required={true}
+        />
+        <TextField
+          id="standard-email"
+          name="email"
+          label="Email"
+          type="email"
+          className={classes.textField}
+          value={this.state.email}
+          onChange={this.handleChange('email')}
+          margin="normal"
+          required={true}
+        />
+        <TextField
+          id="standard-multiline-static"
+          label="Multiline"
+          multiline
+          rows="4"
+          className={classes.textField}
+          margin="normal"
+          required={true}
+          onChange={this.handleChange('message')}
+          value={this.state.message}
+        />
+        <Button variant="contained" color="primary" className={classes.button} type="submit">
+          Submit
+        </Button>
+      </form>
     );
   }
 }
+
+Form.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Form);
